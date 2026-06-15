627a254e306d435494ef54d04b4bfc57d3374729
Abiyyu Nizar <abiyyunizar@gmail.com>
2026-06-15 15:37:04 +0700

    fix: switch to MariaDB adapter for Prisma

--- api/src/lib/prisma.ts
+++ api/src/lib/prisma.ts
@@ -1,6 +1,10 @@
 import { PrismaClient } from '@prisma/client';
+import { PrismaMariaDb } from '@prisma/adapter-mariadb';
+
+const adapter = new PrismaMariaDb(process.env['DATABASE_URL'] || '');
 
 const prisma = new PrismaClient({
+  adapter,
   log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
 });
 
